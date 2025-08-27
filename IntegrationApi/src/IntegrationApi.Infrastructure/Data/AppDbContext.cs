f68c40f1643d90a25d132d4759db544f955f9be0
Lucas Luan B Barbosa <96320917+luc-llb@users.noreply.github.com>
2025-08-27 19:02:59 +0000

    refactor: uses Reflection to add and persist entities with AppDbContext

--- IntegrationApi/src/IntegrationApi.Infrastructure/Data/AppDbContext.cs
+++ IntegrationApi/src/IntegrationApi.Infrastructure/Data/AppDbContext.cs
@@ -1,20 +1,40 @@
 using Microsoft.EntityFrameworkCore;
 using IntegrationApi.Core.Entities;
+using System.Reflection;
 
 namespace IntegrationApi.Infrastructure.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        
+
         // Construtor sem parâmetros para o EF Core Design Time
         public AppDbContext() { }
-        
-        public DbSet<Character> Characters => Set<Character>();
-        public DbSet<Animation> Animations => Set<Animation>();
-        
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        // public DbSet<Character> Characters => Set<Character>();
+        // public DbSet<Animation> Animations => Set<Animation>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            var entityTypes = Assembly.GetAssembly(typeof(BaseEntity))?
+                .GetTypes()
+                .Where(t => t.IsClass && !t.IsAbstract && typeof(BaseEntity).IsAssignableFrom(t));
+
+            if (entityTypes != null)
+            {
+                foreach (var type in entityTypes)
+                {
+                    modelBuilder.Entity(type);
+                }
+            }else
+            {
+                throw new InvalidOperationException("No entity types found in the assembly.");
+            }
         }
+        
     }
 }
