d629795a63d8ce79e90f97cdf7999f7ae6b68f7d
Lucas Luan B Barbosa <96320917+luc-llb@users.noreply.github.com>
2025-08-27 17:42:41 +0000

    remove: remove ICharacterRepository and CharacterRepository implementations

--- IntegrationApi/src/IntegrationApi.Core/Interfaces/ICharacterRepository.cs
+++
@@ -1,9 +0,0 @@
-using IntegrationApi.Core.Entities;
-
-namespace IntegrationApi.Core.Interfaces
-{
-    public interface ICharacterRepository
-    {
-        Task<IEnumerable<Character>> GetAllAsync();
-    }
-}
