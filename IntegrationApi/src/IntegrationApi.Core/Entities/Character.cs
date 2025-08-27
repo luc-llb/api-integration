9fdf03b335c516f4583ad95c7c813436ed5b229a
Lucas Luan B Barbosa <96320917+luc-llb@users.noreply.github.com>
2025-08-27 17:46:59 +0000

    refactor: rewrite the Character class using annotations and the BaseEntity

--- IntegrationApi/src/IntegrationApi.Core/Entities/Character.cs
+++ IntegrationApi/src/IntegrationApi.Core/Entities/Character.cs
@@ -1,13 +1,32 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 namespace IntegrationApi.Core.Entities
 {
-    public class Character
+    public class Character : BaseEntity
     {
-        [Key]
-        public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        [AllowNull]
+        public string Gender { get; set; }
+        [AllowNull]
+        public DateTime DateOfBirth { get; set; }
+        [AllowNull]
+        public string[] AlternativeName { get; set; }
+        [AllowNull]
+        [ForeignKey(nameof(Animation))]
+        public int AnimationId { get; set; }
+
+        public Character(int id, string name, string gender, DateTime dateOfBirth, string[] alternativeName, int animationId)
+        {
+            Id = id;
+            Name = name;
+            Gender = gender ?? "Unknown";
+            DateOfBirth = dateOfBirth;
+            AlternativeName = alternativeName ?? Array.Empty<string>();
+            AnimationId = animationId;
+        }
+
     }
 }
