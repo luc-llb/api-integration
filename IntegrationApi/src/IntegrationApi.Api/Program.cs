e360994eebd378cbace9af65c60016598e504530
Lucas Luan B Barbosa <96320917+luc-llb@users.noreply.github.com>
2025-08-27 03:01:51 +0000

    feat: implement dynamic database connection string based on environment variables

--- IntegrationApi/src/IntegrationApi.Api/Program.cs
+++ IntegrationApi/src/IntegrationApi.Api/Program.cs
@@ -1,9 +1,46 @@
+using DotNetEnv;
+using IntegrationApi.Infrastructure.Data;
+using IntegrationApi.Core.Interfaces;
+using IntegrationApi.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+Env.Load();
+
+var dbType = Environment.GetEnvironmentVariable("DB_TYPE");
+var host = Environment.GetEnvironmentVariable("DB_HOST");
+var port = Environment.GetEnvironmentVariable("DB_PORT");
+var user = Environment.GetEnvironmentVariable("DB_USER");
+var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+
+// Para SQL Server, a string de conexão é diferente
+string connectionString;
+if (dbType == "sqlserver")
+{
+    connectionString = $"Server={host},{port};Database={dbName};User Id={user};Password={password};TrustServerCertificate=True;";
+}
+else
+{
+    connectionString = $"Host={host};Port={port};Username={user};Password={password};Database={dbName}";
+}
+
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+if (dbType == "sqlserver")
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(connectionString));
+}
+else
+{
+    throw new Exception("Database type not supported");
+}
+
+builder.Services.AddScoped<ICharacterRepository, CharacterRepository>();
 
 var app = builder.Build();
 
