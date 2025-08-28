1ae424118bd6d18e7924f5d1df5688c128893ede
Lucas Luan B Barbosa <96320917+luc-llb@users.noreply.github.com>
2025-08-28 19:38:43 +0000

    feat: add AniListId property and update constructors in Animation and Character classes

--- IntegrationApi/src/IntegrationApi.Core/Entities/Animation.cs
+++ IntegrationApi/src/IntegrationApi.Core/Entities/Animation.cs
@@ -8,25 +8,38 @@ namespace IntegrationApi.Core.Entities
     /// </summary>
     public class Animation : BaseEntity
     {
-
         [Required]
         public string Title { get; set; }
+        
         [AllowNull]
         public int Episodes { get; set; }
+        
         [AllowNull]
         public int Duration { get; set; }
+        
         [AllowNull]
         public string[] Genres { get; set; }
+        
         [AllowNull]
         public string Season { get; set; }
+        
         [AllowNull]
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        
         [AllowNull]
         public string[] AlternativeTitle { get; set; }
 
-        public Animation(int id, string title, int episodes, int duration, string[] genres, string season, DateTime endDate, string[] alternativeTitle)
+        public Animation()
         {
-            Id = id;
+            Title = string.Empty;
+            Genres = Array.Empty<string>();
+            Season = "Unknown";
+            AlternativeTitle = Array.Empty<string>();
+        }
+
+        public Animation(int aniListId, string title, int episodes, int duration, string[] genres, string season, DateTime? endDate, string[] alternativeTitle)
+        {
+            AniListId = aniListId;
             Title = title;
             Episodes = episodes;
             Duration = duration;
